9a793142b65a2228af6b6486dc553f59b758b388
Carlos Meneses <carlosahz21@gmail.com>
2020-12-09 09:28:57 -0500

    Vrsion inicial

---
+++ src/main/java/com/clearminds/cmh/middleware/App.java
@@ -0,0 +1,13 @@
+package com.clearminds.cmh.middleware;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
